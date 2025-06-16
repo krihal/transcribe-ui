64edbb1578730d03db6e58b744f4775e15d15ec9
Kristofer Hallin <kristofer@sunet.se>
2025-06-16 14:43:32 +0200

    Better handling of tokens.

--- utils/token.py
+++ utils/token.py
@@ -4,55 +4,45 @@ import time
 
 from nicegui import app
 from nicegui import ui
+from typing import Optional
 from utils.settings import get_settings
 
 settings = get_settings()
 
 
-def token_refresh() -> None:
+def token_refresh(no_redirect: Optional[bool] = False) -> None:
     """
     Refresh the token using the refresh token.
     """
 
-    auth_token = app.storage.user.get("token")
-    refresh_token = app.storage.user.get("refresh_token")
+    token_auth = app.storage.user.get("token")
+    token_refresh = app.storage.user.get("refresh_token")
 
     try:
-        # Get the expiration time from the JWT
         jwt_instance = jwt.JWT()
-        jwt_decoded = jwt_instance.decode(auth_token, do_verify=False)
+        jwt_decoded = jwt_instance.decode(token_auth, do_verify=False)
     except Exception:
-        # Force the user to log out if the token is invalid
-        ui.navigate.to(settings.OIDC_APP_LOGOUT_ROUTE)
+        if not no_redirect:
+            ui.navigate.to(settings.OIDC_APP_LOGOUT_ROUTE)
         return
 
-    # Only refresh if the token is about to expire
-    # within 20 seconds.
+    # Only refresh if the token is about to expire within 20 seconds.
     if jwt_decoded["exp"] - int(time.time()) > 20:
         return
 
     try:
-        # Make a request to refresh the token
         response = requests.post(
             settings.OIDC_APP_REFRESH_ROUTE,
-            json={"token": refresh_token},
+            json={"token": token_refresh},
         )
         response.raise_for_status()
     except requests.exceptions.RequestException:
-        ui.navigate.to(settings.OIDC_APP_LOGOUT_ROUTE)
+        if not no_redirect:
+            ui.navigate.to(settings.OIDC_APP_LOGOUT_ROUTE)
         return
 
     token = response.json().get("access_token")
     app.storage.user["token"] = token
-
-    if settings.API_DEBUG:
-        ui.notification(
-            "Token refreshed successfully.",
-            color="green",
-            icon="check_circle",
-            position="bottom-right",
-            timeout=2,
-        )
 
 
 def get_auth_header() -> dict[str, str]:
@@ -60,15 +50,14 @@ def get_auth_header() -> dict[str, str]:
     Get the authorization header for API requests.
     """
 
+    token_refresh(no_redirect=True)
     token = app.storage.user.get("token")
 
     try:
         jwt_instance = jwt.JWT()
         jwt_instance.decode(token, do_verify=False)
     except Exception:
-        # Fetch a new token if we for some reason have an invalid token
-        ui.navigate.to(settings.OIDC_APP_LOGOUT_ROUTE)
-        return {}
+        return None
 
     return {"Authorization": f"Bearer {token}"}
 
